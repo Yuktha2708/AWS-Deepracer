e2e33bca272010a8f2f11117c36acc32af7ea72a
Yuktha2708 <76445341+Yuktha2708@users.noreply.github.com>
2022-07-18 18:43:13 +0530

    Create Whoa-Model.py
    Combined follow line and within border

---
+++ Whoa-Model.py
@@ -0,0 +1,36 @@
+def reward_function(params):
+    # Example of rewarding the agent to stay inside the two borders of the track
+    
+    # Read input parameters
+    all_wheels_on_track = params['all_wheels_on_track']
+    distance_from_center = params['distance_from_center']
+    track_width = params['track_width']
+    
+    # Give a very low reward by default
+    reward = 1e-3
+    
+    # Calculate 3 markers that are at varying distances away from the center line
+    marker_1 = 0.1 * track_width
+    marker_2 = 0.25 * track_width
+    marker_3 = 0.5 * track_width
+
+    # Give higher reward if the car is closer to center line and vice versa
+    if distance_from_center <= marker_1:
+     reward = 1.0
+    elif distance_from_center <= marker_2:
+     reward = 0.5
+    elif distance_from_center <= marker_3:
+     reward = 0.1
+    else:
+     reward = 1e-3  # likely crashed/ close to off track
+
+
+    # Give a high reward if no wheels go off the track and
+    # the agent is somewhere in between the track borders
+    if all_wheels_on_track and (0.2*track_width - distance_from_center) >= 0.05:
+        reward = 1.0
+        
+    
+
+    # Always return a float value
+    return float(reward)
