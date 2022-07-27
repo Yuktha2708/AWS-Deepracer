a35cbe796a53fe01f311708229937927ff0f1f64
Yuktha2708 <76445341+Yuktha2708@users.noreply.github.com>
2022-07-28 04:13:12 +0530

    Create ideology-V1.py
    Be withing tracks and circle methodology implemented

---
+++ ideology-V1.py
@@ -0,0 +1,27 @@
+def reward_function(params):
+    # Example of rewarding the agent to stay inside the two borders of the track
+    
+    # Read input parameters
+    all_wheels_on_track = params['all_wheels_on_track']
+    distance_from_center = params['distance_from_center']
+    track_width = params['track_width']
+    track_length = params['track_length']
+    
+    radius = track_length*0.16
+    
+    # Give a very low reward by default
+    reward = 1e-3
+    
+    if radius<(radius - track_width - distance_from_center):
+        reward= 1.0
+        
+    else:
+        reward = 1e-3
+    
+    # Give a high reward if no wheels go off the track and
+    # the agent is somewhere in between the track borders
+    if all_wheels_on_track and (0.5*track_width - distance_from_center) >= 0.05:
+        reward = 3.0
+
+    # Always return a float value
+    return float(reward)
