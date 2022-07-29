98450bd847b79296c33df57c297535a4136ef000
Yuktha2708 <76445341+Yuktha2708@users.noreply.github.com>
2022-07-30 02:41:36 +0530

    Create ideology-V2.py
    is left function, r-d_f_c

---
+++ ideology-V2.py
@@ -0,0 +1,34 @@
+def reward_function(params):
+    # Example of rewarding the agent to stay inside the two borders of the track
+    
+    # Read input parameters
+    all_wheels_on_track = params['all_wheels_on_track']
+    distance_from_center = params['distance_from_center']
+    track_width = params['track_width']
+    track_length = params['track_length']
+    is_left_of_center = params['is_left_of_center']
+    
+    radius = track_length*0.16
+    
+    
+    # Give a high reward if no wheels go off the track and
+    # the agent is somewhere in between the track borders
+    if all_wheels_on_track and (0.5*track_width - distance_from_center) >= 0.05:
+        reward = 3.0
+
+    
+    # Give a very low reward by default
+    reward = 1e-3
+    
+    if is_left_of_center == True:
+        if radius<(radius - distance_from_center):
+            reward= 2.0
+        else:
+            reward = 1e-3
+    else:
+            reward = 1e-3
+    
+   
+
+    # Always return a float value
+    return float(reward)
