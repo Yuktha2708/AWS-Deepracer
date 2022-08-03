cb66eaa6814429895bc0d0c749793ca6fe563cb7
Yuktha2708 <76445341+Yuktha2708@users.noreply.github.com>
2022-08-04 00:17:14 +0530

    is left, traction, steps

--- tam/yuktha-test-clone.py
+++ tam/yuktha-test-clone.py
@@ -1,30 +1,39 @@
 def reward_function(params):
-
-    # Example of rewarding the agent to follow center line
-
+    # Example of rewarding the agent to stay inside the two borders of the track
+    
     # Read input parameters
-
-    track_width = params['track_width']
+    all_wheels_on_track = params['all_wheels_on_track']
     distance_from_center = params['distance_from_center']
+    track_width = params['track_width']
+    track_length = params['track_length']
+    is_left_of_center = params['is_left_of_center']
+    steps = params['steps']
+    progress = params['progress']
 
-    # Calculate 3 markers that are at varying distances away from the center line
+    
+    radius = track_length*0.16
+    
+    
+    # Give a high reward if no wheels go off the track and
+    # the agent is somewhere in between the track borders
+    if all_wheels_on_track and (0.5*track_width - distance_from_center) >= 0.05:
+        reward = 3.0
 
-    marker_1 = 0.1 * track_width
-    marker_2 = 0.25 * track_width
-    marker_3 = 0.5 * track_width
-
-    # Give higher reward if the car is closer to center line and vice versa
-    if distance_from_center <= marker_1:
-
-        reward = 1.0
-    elif distance_from_center <= marker_2:
-
-        reward = 0.5
-    elif distance_from_center <= marker_3:
-
-        reward = 0.1
+    
+    # Give a very low reward by default
+    reward = 1e-3
+    
+    if is_left_of_center == True:
+        if radius<(radius - distance_from_center):
+            reward= 2.0
+        else:
+            reward = 1e-3
     else:
-        reward = 1e-3 # likely crashed/ close to off track
+            reward = 1e-3
+    
+    TOTAL_NUM_STEPS = 250
+    if (steps % 100) == 0 and progress > (steps / TOTAL_NUM_STEPS) * 100 :
+        reward += 10.0
 
-
+    # Always return a float value
     return float(reward)
